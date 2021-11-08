42f7b4e8eb4fc221b32c60dcf6fd46f3a22b2126
JunyTM <20010784@st.phenikaa-uni.edu.vn>
2021-11-08 21:30:56 +0700

    update

--- quickSort2.c
+++ quickSort2.c
@@ -52,13 +52,6 @@ void print_array(int k[], int n)
 
 int main(void)
 {
-    // Cho khai báo của hàm sắp xếp dãy số nguyên theo thứ tự tăng dần dùng giải thuật sắp xếp -
-    // Quicksort.LB là chỉ số của phần tử đầu,
-
-    // UB là chỉ số phần tử cuối của dãy cần sắp xếp.void QuickSort(int K[], int LB, int UB);
-    // Cho khai báo hàm phân đoạn dãy K có chỉ số phần tử đầu là LB, chỉ số phần tử cuối là UB,
-    // chọn phần tử chốt là phần tử đầu.Hàm trả về vị trí của phần tử chốt sau khi thực hiện phân
-    // đoạn.
 
     int K[12] = {0, 42, 23, 74, 11, 65, 58, 94, 36, 99, 87, 30000};
 
