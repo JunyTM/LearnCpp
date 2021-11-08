96d0a501d55c7d1d4f893a11281f36ee62429006
JunyTM <20010784@st.phenikaa-uni.edu.vn>
2021-11-08 17:52:15 +0700

    heapsSort

--- heapsSort.c
+++ heapsSort.c
@@ -1,47 +1,70 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
-void ADJUST(int K[],int i,int n){
+/* Bug tạo đống sẽ bỏ qua phần từ K[0] ~ phần tử đầu tiền
+=> Ta sẽ chỉ thao tác với n - 1 phần tử,
+	duyệt từ phần tử thứ 2 trong mảng.
+*/
+void printArray(int K[], int n)
+{
+	int i;
+	for (i = 1; i < n; i++)
+	{
+		printf("%d ", K[i]);
+	}
+}
+
+void ADJUST(int K[], int i, int n) // i là chỉ số nút gốc, n là số nút của cây
+{
 	int KEY = K[i];
-	int j=2*i;
-	while(j<=n){
-		if((j<n)&&(K[j] < K[j+1])) j = j+1;
-		if(KEY > K[j]){
-			K[j/2] = KEY;
+	int j = 2 * i; // j ghi nhận số thứ tự Nút Con Trái của i
+
+	while (j <= n)
+	{
+		if ((j < n) && (K[j] < K[j + 1])) // tìm vịt trí nút còn phải (nút con lớn nhất) để so sánh với nút gốc(KEY)
+			j = j + 1;
+
+		if (KEY > K[j])
+		{
+			K[j / 2] = KEY; // khi xác định được vị trí của KEY => dừng lại
 			return;
 		}
-		K[j/2] = K[j];
-		j = 2*j;
+		K[j / 2] = K[j]; // Nếu KEY nhỏ hơn khóa con => đưa khóa con lên
+
+		j = 2 * j; // Tiếp tục đi xuống nhánh dưới để tìm vị trí cho KEY, vì KEY có thể nhỏ hợn nút ở nhánh này
 	}
-	K[j] = KEY;
+	K[j / 2] = KEY;
 }
 
-void HeapSort(int K[], int n){
+void HeapSort(int K[], int n)
+{
 	int i;
-	for(i= n/2; i>=1;i--){
-		ADJUST(K,i,n);
+	// Tạo đống
+	for (i = n / 2; i >= 1; i--)
+	{
+		ADJUST(K, i, n);
 	}
-	for(i=n-1;i>=1;i--){
+	for (i = n - 1; i >= 1; i--)
+	{
+		// Đổi chỗ phần từ đầu với phần tử cuối
 		int tmp = K[1];
-		K[1] = K[i+1];
-		K[i+1] = tmp;
-		ADJUST(K,1,i);
+		K[1] = K[i + 1];
+		K[i + 1] = tmp;
+
+		ADJUST(K, 1, i);
 	}
 }
 
-void printArray(int K[],int n){
-	int i;
-	for(i=0;i<n;i++){
-		printf("%d ",K[i]);
-	}
-}
-
-int main(){
+int main()
+{
 	int K[] = {2, 9, 3, 6, 1, 4, 5, 7, 0, 8};
-	int n = sizeof(K)/sizeof(int);
+	int n = sizeof(K) / sizeof(int); // Kích thước mảng
+
 	printf("Mang truoc khi sap xep: ");
-	printArray(K,n);
-	HeapSort(K,n);
+	printArray(K, n);
+
+	HeapSort(K, n);
+
 	printf("\nMang sau khi sap xep: ");
-	printArray(K,n);
+	printArray(K, n);
 }
---
+++ heapsort_Hòa.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Phân đống
+void ADJUST(int K[], int i, int n) // i vị trí nút gốc, n số lượng nút
+{
+    int KEY = K[i];
+    int j = 2 * i;
+    while (j <= n)
+    {
+        if ( (j < n) && (K[j] < K[j+1]) ) j = j + 1;
+        if (KEY > K[j])
+        {
+            K[j/2] = KEY;
+            return;
+        }
+        K[j/2] = K[j];
+        j = 2 * j ;
+    }
+    K[j/2] = KEY;
+}
+
+// Bug bỏ qua phần tử vị trí K[0]
+void HeapSort(int K[], int n)
+{
+    int i;
+    for (i = n/2 ; i >=1; i--)
+        ADJUST(K, i, n);
+    for (i = n - 1; i >= 1; i--)
+    {
+        int tmp = K[1];
+        K[1] = K[i+1] ;
+        K[i+1] = tmp;
+        ADJUST(K, 1, i);
+    }
+}
+
+// In ra mảng trừ phần tử đầu tiên 
+void print_array(int k[], int n)
+{
+    int i;   
+    for (i = 1; i < n; i++)
+    {
+        printf("%d ", k[i]);
+    }
+}
+int main(void)
+{
+    int K[12]= {9,8,1,3,2,0,7,6,5,4,12,11};
+    printf("\nMang truoc khi sap xep: ");
+    print_array(K, 12); 
+    HeapSort(K, 11);
+    printf("\nMang sau khi sap xep: ");
+    print_array(K, 12);
+    return 0;
+}
